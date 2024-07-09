10b03ab70cbc26cf95366ead07ba50ba5d0608cd
Victor Aguilar <39719521+hugoaguilar99@users.noreply.github.com>
2024-07-08 20:15:48 -0600

    Add project files.

---
+++ ExamenTecnicoAlsuper/Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="background-color:blueviolet; font-size:xx-large; color:white"  align="center">
+        Employees
+    </div>
+    <br />
+    <div align="center" style-="padding:15px">
+        <br />
+        <table  >
+            <tr>
+                <td>&nbsp;<asp:Label runat="server" Text="Employee Id" ID="Label2"></asp:Label></td>
+                <td>&nbsp;<asp:TextBox runat="server" ID="Text_Id"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>&nbsp;<asp:Label runat="server" Text="Name" ID="ctl02"></asp:Label></td>
+                <td>&nbsp;<asp:TextBox runat="server" ID="Text_Name"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>&nbsp;<asp:Label runat="server" Text="LastName P" ID="ctl03"></asp:Label></td>
+                <td>&nbsp;<asp:TextBox runat="server" ID="Text_LastP"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>&nbsp;<asp:Label runat="server" Text="LastName M" ID="Label1"></asp:Label></td>
+                <td>&nbsp;<asp:TextBox runat="server" ID="Text_LastM"></asp:TextBox></td>
+            </tr>
+        </table>
+        <div align="center">
+            <asp:Button runat="server" Text="Add" Width="75px" ID="Btn_Add"></asp:Button>
+            <asp:Button runat="server" Text="Update" Width="75px" ID="Btn_Update"></asp:Button>
+            <asp:Button runat="server" Text="Delete" Width="75px" ID="Btn_Delete"></asp:Button>
+        </div>
+        <br />
+        
+        <div align="center">
+            <hr />
+            <asp:GridView runat="server" ID="GridView1" Width="80%"></asp:GridView>
+        </div>
+        <br />
+
+        <div align="center">
+            <hr />
+            <asp:GridView runat="server" ID="Gridview2" Width="80%"></asp:GridView>
+            
+        </div>
+<br />
+
+    </div>
+</asp:Content>
---
+++ ExamenTecnicoAlsuper/Default.aspx.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Partial Class _Default
+    Inherits Page
+    Dim connect As New SqlConnection("Data Source=DESKTOP-DN21R3B\MSSQLSERVER01;Initial Catalog=BD_CRUD;User ID=test;Password=123456789")
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ListProduct()
+    End Sub
+
+    Protected Sub Btn_Add_Click(sender As Object, e As EventArgs) Handles Btn_Add.Click
+        Dim cmd As New SqlCommand
+        Dim cmd2 As New SqlCommand
+        connect.Open()
+        cmd = New SqlCommand("Insert_Employee", connect)
+        cmd2 = New SqlCommand("Insert_Mov", connect)
+        cmd.CommandType = 4
+        cmd2.CommandType = 4
+
+        cmd2.Parameters.AddWithValue("@mov_id", CInt(Text_Id.Text.ToString))
+        cmd2.Parameters.AddWithValue("@mov_type", "Alta")
+
+        cmd.Parameters.AddWithValue("@idEmployee", CInt(Text_Id.Text.ToString))
+        cmd.Parameters.AddWithValue("@name", Text_Name.Text.ToString)
+        cmd.Parameters.AddWithValue("@lastP", Text_LastP.Text.ToString)
+        cmd.Parameters.AddWithValue("@lastM", Text_LastM.Text.ToString)
+        cmd.ExecuteNonQuery()
+        cmd2.ExecuteNonQuery()
+        MsgBox("succesfully Inserted", MsgBoxStyle.Information, "Message")
+        ListProduct()
+        connect.Close()
+
+    End Sub
+
+    Private Sub ListProduct()
+        Dim cmd As New SqlCommand
+        cmd = New SqlCommand("Show_Employees", connect)
+        Dim sd As New SqlDataAdapter(cmd)
+        Dim dt As New DataTable
+        sd.Fill(dt)
+        GridView1.DataSource = dt
+        GridView1.DataBind()
+
+        Dim cmd2 As New SqlCommand
+        cmd2 = New SqlCommand("Read_Mov", connect)
+        Dim sd2 As New SqlDataAdapter(cmd2)
+        Dim dt2 As New DataTable
+        sd2.Fill(dt2)
+        Gridview2.DataSource = dt2
+        Gridview2.DataBind()
+    End Sub
+
+
+    Protected Sub Btn_Update_Click(sender As Object, e As EventArgs) Handles Btn_Update.Click
+        Dim cmd As New SqlCommand
+        Dim cmd2 As New SqlCommand
+        connect.Open()
+        cmd = New SqlCommand("Edit_Employee", connect)
+        cmd2 = New SqlCommand("Insert_Mov", connect)
+        cmd.CommandType = 4
+        cmd2.CommandType = 4
+
+        cmd2.Parameters.AddWithValue("@mov_id", CInt(Text_Id.Text.ToString))
+        cmd2.Parameters.AddWithValue("@mov_type", "Cambios")
+
+        cmd.Parameters.AddWithValue("@idEmployee", CInt(Text_Id.Text.ToString))
+        cmd.Parameters.AddWithValue("@name", Text_Name.Text.ToString)
+        cmd.Parameters.AddWithValue("@lastP", Text_LastP.Text.ToString)
+        cmd.Parameters.AddWithValue("@lastM", Text_LastM.Text.ToString)
+        cmd.ExecuteNonQuery()
+        cmd2.ExecuteNonQuery()
+        MsgBox("succesfully Updated", MsgBoxStyle.Information, "Message")
+        ListProduct()
+        connect.Close()
+    End Sub
+
+    Protected Sub Btn_Delete_Click(sender As Object, e As EventArgs) Handles Btn_Delete.Click
+        Dim cmd As New SqlCommand
+        Dim cmd2 As New SqlCommand
+        connect.Open()
+        cmd = New SqlCommand("Delete_Employee", connect)
+        cmd2 = New SqlCommand("Insert_Mov", connect)
+        cmd.CommandType = 4
+        cmd2.CommandType = 4
+
+        cmd2.Parameters.AddWithValue("@mov_id", CInt(Text_Id.Text.ToString))
+        cmd2.Parameters.AddWithValue("@mov_type", "Baja")
+
+        cmd.Parameters.AddWithValue("@idEmployee", CInt(Text_Id.Text.ToString))
+        cmd.ExecuteNonQuery()
+        cmd2.ExecuteNonQuery()
+        MsgBox("succesfully Deleted", MsgBoxStyle.Information, "Message")
+        ListProduct()
+        connect.Close()
+    End Sub
+End Class
